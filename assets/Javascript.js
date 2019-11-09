17abdf443b2bef379bd6120593817cf93a932632
Mathew Ramirez <Mathew.J.Ramirez@gmail.com>
2019-11-09 13:23:04 -0600

    Added boilerplate code to store train info and display it

--- assets/Javascript.js
+++ assets/Javascript.js
@@ -3,11 +3,37 @@ var trainTime = "";
 var destinationName = "";
 var firstTrainTime = "";
 var trainFrequency = "";
+var trains = [];
 
 //this function stores all data
 function storeData() {
-    trainName = $('Train-Name').val();
-    destinationName = $('Destination-Name').val();
-    firstTrainTime = $('Train-Time').val();
-    trainFrequency = $('Frequency').val();
+    trainName = $('#Train-Name').val();
+    destinationName = $('#Destination-Name').val();
+    firstTrainTime = $('#First-Time').val();
+    trainFrequency = $('#Frequency').val();
 }
+
+//this creates a new train
+function newTrain() {
+    trains.push(trainName = {
+        trainName, 
+        destinationName,
+        firstTrainTime,
+        trainFrequency
+    })
+}
+
+//this function displays train info
+function displayTrainInfo() {
+    for (var i = 0; i < trains.length; i++) {
+        console.log(trains[i].trainName);
+        $('#trainInfo').append(trains[i].trainName);
+    }
+}
+
+$(".addTrain").on("click", function(event) {
+    event.preventDefault();
+    storeData();
+    newTrain();
+    displayTrainInfo();
+});
