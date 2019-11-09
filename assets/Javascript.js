66f7f72e51436140dbc90cea1771a0150bdc35b9
Mathew Ramirez <Mathew.J.Ramirez@gmail.com>
2019-11-09 12:47:24 -0600

    Added more to javascript for basic function

--- assets/Javascript.js
+++ assets/Javascript.js
@@ -1,1 +1,13 @@
-//sample code
+//beginning variable initialization
+var trainTime = "";
+var destinationName = "";
+var firstTrainTime = "";
+var trainFrequency = "";
+
+//this function stores all data
+function storeData() {
+    trainName = $('Train-Name').val();
+    destinationName = $('Destination-Name').val();
+    firstTrainTime = $('Train-Time').val();
+    trainFrequency = $('Frequency').val();
+}
